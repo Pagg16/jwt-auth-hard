64ac411f594d76baf1e94da28bcc6c33143833dc
Pavel Kulebin <pagg16pagg@gmail.com>
2023-04-06 19:23:04 +0300

    server start

---
+++ server/controller/userController.js
@@ -0,0 +1,13 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (e) {}
+  }
+}
+
+module.exports = new UserController();
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const router = require("./router");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api", router);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    app.listen(PORT, console.log("Server startned on " + PORT));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
